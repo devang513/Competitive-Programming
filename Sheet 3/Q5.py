e1c7931ea9ff9080e9619f780ae5dcc3bd3c71c7
devang513 <devangbansal1109@gmail.com>
2025-09-03 23:21:31 +0530

    Add files via upload

---
+++ Sheet 3/Q5.py
@@ -0,0 +1,3 @@
+n = int(input("Enter rows: "))
+for i in range(n):
+    print("*", "_ "*(n-2), "*")
---
+++ Sheet 3/Q6.py
@@ -0,0 +1,3 @@
+n = int(input("Enter rows: "))
+for i in range(n,0,-1):
+    print("*", "_ "*(i-1), "*")
